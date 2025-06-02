7d14e42d98593f9e9ff2cebca4230076c8ea3b3c
김도현 <153271427+hyeonn-06@users.noreply.github.com>
2025-06-03 00:26:57 +0900

    Add files via upload

---
+++ src/member/ask.js
@@ -0,0 +1,11 @@
+const Ask = () => {
+    return(
+        <div>
+            <form>
+                <input></input>
+                <button>질문</button>
+            </form>
+        </div>
+    )
+}
+export default Ask;
