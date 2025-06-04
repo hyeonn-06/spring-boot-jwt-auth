baaba170f28cc7e94b4163efbccc02f384b74eb7
김도현 <153271427+hyeonn-06@users.noreply.github.com>
2025-06-04 14:35:03 +0900

    Add files via upload

---
+++ src/member/ask.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { useState } from "react";
+import { getTokenFromCookie } from "../utils/cookieUtils";
+
+const Ask = () => {
+    const [question, setQuestion] = useState("");
+    const accessToken = getTokenFromCookie("accessToken");
+    const refreshToken = getTokenFromCookie("refreshToken");
+
+    const handleQuestionChange = (e) => {
+        setQuestion(e.target.value);
+    };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const requestUrl = `http://localhost/member/chatbot/ask.do`;
+        try {
+            const response = await axios.post(requestUrl, {question}, {
+                headers: {Authorization: `Bearer ${accessToken}`, 'X-Refresh-Token': refreshToken, 'Content-Type': 'application/json'}
+            });
+            alert(response.data);
+        } catch (error) {
+            alert("알 수 없는 오류 발생" + error);
+        }
+    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input onChange={handleQuestionChange}></input>
+                <button>질문</button>
+            </form>
+        </div>
+    )
+}
+export default Ask;
