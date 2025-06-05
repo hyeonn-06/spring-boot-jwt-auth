bd5580e449d12915adfff2818970e357ae3c248f
김도현 <153271427+hyeonn-06@users.noreply.github.com>
2025-06-05 11:50:03 +0900

    Delete helper directory

--- helper/src/main/java/com/helper/auth/mapper/RefreshTokenMapper.java
+++
@@ -1,8 +0,0 @@
-package com.helper.auth.mapper;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface RefreshTokenMapper {
-	public Integer validateRefreshToken(String token);
-}
